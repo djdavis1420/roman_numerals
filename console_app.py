ee85584a836e1935addfbd5894d0074908c06e8c
Dylan Davis <djdavis1420@gmail.com>
2019-05-20 18:52:26 -0500

    Added Basic Console App

---
+++ console_app.py
@@ -0,0 +1,25 @@
+#!/C/Users/djdav/Development/Python/roman_numerals/venv/Scripts/python
+
+
+from src.models.arabic_to_roman import ArabicToRomanConverter
+from src.models.roman_to_arabic import RomanToArabicConverter
+
+
+def console_app():
+    input_string = input("Enter a valid Roman Numeral or Arabic Number.\nInput: ")
+    translate(input_string)
+
+
+def translate(input_string):
+    try:
+        number = int(input_string)
+        converter = ArabicToRomanConverter()
+        result = converter.convert(number)
+        print("Output: " + result)
+    except ValueError:
+        converter = RomanToArabicConverter()
+        result = converter.convert(input_string)
+        print("Output: " + str(result))
+
+
+console_app()
